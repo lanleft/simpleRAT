97c342d0274c27c5f04a6e3232cbb4eba9de587c
lanleft <50655448+lanvuit2@users.noreply.github.com>
2020-09-23 20:28:44 +0700

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <iostream>
+#include <processthreadsapi.h>
+#include <winreg.h>
+#include <stdlib.h>
+#include <Shlwapi.h>
+#include <string.h>
+#include <ShlObj_core.h>
+
+#include <PathCch.h>
+#include <compressapi.h>
+#include <PathCch.h>
+#include <stdlib.h>
